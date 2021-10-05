153a1658c43c8bb883cf0ab8b26b069b5d1dba29
SovaV <sova.volodymyr95@gmail.com>
2021-10-05 17:59:12 +0300

    fetch

--- src/component/App.js
+++ src/component/App.js
@@ -2,25 +2,65 @@ import React, { Component } from 'react';
 import Container from '../component/Container/Container';
 //====================================
 import Searchbar from './Searchbar/Searchbar';
-import axios from 'axios';
+// import ImageGallery from './ImageGallery/ImageGallery';
 
 class App extends Component {
   state = {
-    total: 4692,
+    text: '',
+    // page: 1,
+    images: [],
+    loading: false,
   };
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevState.text;
+    const nextName = this.state.text;
+    const API_KEY = '22963284-23f543f8627e95ac39317c785';
 
-  componentDidMount() {
-    axios
-      .get('https://hn.algolia.com/api/v1/search?query=react')
-      .then(response => this.setState({ articles: response.data.articles }))
-      .then(console.log);
+    if (prevName !== nextName) {
+      console.log('изменилось имя');
+
+      this.setState({ loading: true });
+      setTimeout(() => {
+        fetch(
+          `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${nextName}&per_page=12&key=${API_KEY}`,
+        )
+          .then(res => res.json())
+          .then(images => this.setState({ images }))
+          .finally(() => this.setState({ loading: false }));
+      }, 1000);
+    }
   }
+  // fetchContent() {
+  //   const API_KEY = '22963284-23f543f8627e95ac39317c785';
+  //   const BASE_URL = 'https://pixabay.com/api';
+  //   const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.state.text}&page=${this.state.page}&per_page=12&key=${API_KEY}`;
+  //   return fetch(url).then(res => {
+  //     if (res.ok) {
+  //       return res.json();
+  //     }
 
+  //     return Promise.reject(new Error(`Нет покемона с именем ${this.state.text}`));
+  //   });
+  // }
+  handleFormSubmit = text => {
+    this.setState({ text });
+  };
   render() {
+    const { loading, images, text } = this.state;
     return (
       <Container>
-        {/* {this.state.safesearch && <div>{this.state.safesearch}</div>} */}
-        <Searchbar />
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {/* <ImageGallery images={this.state.images} /> */}
+        {loading && <div>Загружаем...</div>}
+        {!images && <div>Введите картинку</div>}
+        {images && (
+          <div>
+            <p>{text}</p>
+            <li>
+              <img src={this.webformatURL} width="600" alt={this.tags} />
+            </li>
+          </div>
+        )}
       </Container>
     );
   }
--- src/component/Searchbar/Searchbar.js
+++ src/component/Searchbar/Searchbar.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import s from './Searchbar.module.css';
 
-const Searchbar = ({ onSubmit }) => (
-  <header className={s.Searchbar}>
-    <form className={s.SearchForm}>
-      <button type="submit" className={s.button}>
-        <span className={s.lable}></span>
-      </button>
+class Searchbar extends Component {
+  state = {
+    text: '',
+  };
 
-      <input
-        className={s.input}
-        type="text"
-        // autocomplete="off"
-        // autofocus
-        placeholder="Search images and photos"
-      />
-    </form>
-  </header>
-);
+  hendleChange = e => {
+    this.setState({ text: e.currentTarget.value.toLowerCase() });
+  };
+
+  hendleSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.text.trim() === '') {
+      alert('Введите имя картинки');
+      return;
+    }
+    this.props.onSubmit(this.state.text);
+    // this.reset();
+  };
+
+  // reset = () => {
+  //   this.setState({ text: '' });
+  // };
+  render() {
+    return (
+      <header className={s.Searchbar}>
+        <form className={s.SearchForm} onSubmit={this.hendleSubmit}>
+          <button type="submit" className={s.button}>
+            <span className={s.lable}></span>
+          </button>
+          <input
+            onChange={this.hendleChange}
+            className={s.input}
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
 
 export default Searchbar;
