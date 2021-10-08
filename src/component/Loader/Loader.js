5d54baf2e65f76133e62dd2c04986292440b13a6
SovaV <sova.volodymyr95@gmail.com>
2021-10-08 14:54:13 +0300

    Update Loader.js

--- src/component/Loader/Loader.js
+++ src/component/Loader/Loader.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import Loader from 'react-loader-spinner';
 export default class Spiner extends Component {
   render() {
-    return (
-      <Loader
-        type="Watch"
-        color="#00BFFF"
-        height={200}
-        width={200}
-        timeout={3000} //3 secs
-      />
-    );
+    return <Loader type="Watch" color="#00BFFF" height={200} width={200} timeout={3000} />;
   }
 }
